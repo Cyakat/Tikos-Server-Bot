ad5ecbb9545487ae2b14cb7ca64e18b3456aa37a
Cyakat <noah@thebauers.net>
2022-04-26 22:36:02 -0600

    added message saying that the vanilla server has whitelist

--- commands/vanilla.js
+++ commands/vanilla.js
@@ -8,6 +8,7 @@ module.exports = {
         embed = new Embed()
         .setTitle('Vanilla Server')
         .setDescription('The Vanilla server ip is \n**mc.tikomc.tk**')
+        .addField('Whitelist','You will need to be on the whitelist\n you can add yourself to the whitelist by using /whitelist')
         .setColor(0x2c93bf)
         interaction.reply({embeds: [embed]});
     }
