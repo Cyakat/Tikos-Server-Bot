1ea00e781bafeaff8a721bdd361b2eba2793aaf2
Cyakat <noah@thebauers.net>
2022-06-07 20:43:23 -0600

    added seed to vanilla command

--- commands/vanilla.js
+++ commands/vanilla.js
@@ -6,11 +6,19 @@ module.exports = {
     .setName('vanilla')
     .setDescription('Replies with the vanilla server ip'),
     async execute(interaction) {
+        row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+            .setLabel('Chunkbase seed map')
+            .setStyle('LINK')
+            .setURL('https://www.chunkbase.com/apps/seed-map#-4789583113264944388')
+        )
         embed = new MessageEmbed()
         .setTitle('Vanilla Server')
         .setDescription('The Vanilla server ip is \n**mc.tikomc.tk**\nIt will always be on the latest version')
         .addField('Whitelist','You will need to be on the whitelist\n you can add yourself to the whitelist by using /whitelist (you username)', true)
+        .addField('Seed','The seed is -4789583113264944388')
         .setColor(0x2c93bf)
-        interaction.reply({embeds: [embed]});
+        interaction.reply({embeds: [embed], components: [row]});
     }
 }
