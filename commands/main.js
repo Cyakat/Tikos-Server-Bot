d602e88e1f7db7206721eec1e77ac234ea3a310e
Cyakat <36944093+Cyakat@users.noreply.github.com>
2022-08-08 12:33:51 -0600

    Update and rename vanilla.js to main.js

--- commands/main.js
+++ commands/main.js
@@ -3,8 +3,8 @@ const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('vanilla')
-    .setDescription('Replies with the vanilla server ip'),
+    .setName('main')
+    .setDescription('Replies with the main server ip'),
     async execute(interaction) {
         row = new MessageActionRow()
         .addComponents(
@@ -15,7 +15,7 @@ module.exports = {
         )
         embed = new MessageEmbed()
         .setTitle('Vanilla Server')
-        .setDescription('The Vanilla server ip is \n**mc.tikomc.tk**\nIt will be on 1.19.0 unless Mojang rolls back their decision')
+        .setDescription('The Main server ip is \n**mc.tikomc.tk**\nIt will be on 1.19.0 unless Mojang rolls back their decision')
         //.addField('Whitelist','You will need to be on the whitelist\n you can add yourself to the whitelist by using /whitelist (you username)', true)
         .addField('Seed','The seed is -4789583113264944388')
         .setColor(0x2c93bf)
