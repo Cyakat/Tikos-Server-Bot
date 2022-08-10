625a3d2cbf9b04bdba9412cd6b526d3a84697457
Cyakat <noah@thebauers.net>
2022-08-10 17:34:16 -0600

    forgot that i changed key names

--- commands/status.js
+++ commands/status.js
@@ -15,12 +15,12 @@ module.exports = {
         let altStatus = 'Down'
         let alt2Status = 'Down'
 
-        exec("ssh 192.168.2.160 -i ~/.ssh/minecraft_key 'pidof java'", (error, stdout, stderr) => {
+        exec("ssh 192.168.2.160 -i ~/.ssh/main_key 'pidof java'", (error, stdout, stderr) => {
             if (stdout != '') {
                 mainStatus = 'Up'
             }
         }); 
-        exec("ssh 192.168.2.161 -i ~/.ssh/modded_key 'pidof java'", (error, stdout, stderr) => {
+        exec("ssh 192.168.2.161 -i ~/.ssh/alt_key 'pidof java'", (error, stdout, stderr) => {
             if (stdout != '') {
                 altStatus = 'Up'
             }
